0f75920cb07172d82b7b8c34e0e8732c6062b10d
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2025-07-06 16:34:22 -0400

    Update layout / Outline future JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+function Task() {
+	// intiailizes a task
+	// change priority
+	// delete task
+}
+
+function Renderer() {
+	// Write setup form to screen
+	// Write tasks to screen
+}
