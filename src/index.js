0bc617b5985aba8e360d04871b5d46fd4314e528
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2025-07-08 09:12:39 -0400

    Move DOM responsibility to Renderer() object
    There is now a separate object for rendering the
    DOM on screen. I also am beginning to implement
    variable priorities

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import dots from "./icons/dots-vertical.svg";
 
 const ADDTASK = document.querySelector(".add-task");
 const TASKCONTAINER = document.querySelector(".container");
+const RENDERER = Renderer();
 
 ADDTASK.addEventListener("click", () => {
 	const newTask = Task();
@@ -12,38 +13,12 @@ ADDTASK.addEventListener("click", () => {
 function Task() {
 	// intiailizes a task
 	const initializeTask = () => {
-		console.log("click");
-		const task = document.createElement("div");
-		task.classList.add("task");
+		let title = "Title";
+		let description = "Description";
+		let dueDate = "01/01/2025";
+		let priority = "High Priority";
 
-		const title = document.createElement("div");
-		title.classList.add("title");
-		title.innerText = "Title";
-
-		const description = document.createElement("div");
-		description.classList.add("description");
-		description.innerText = "Description";
-
-		const dueDate = document.createElement("div");
-		dueDate.classList.add("due-date");
-		dueDate.innerText = "01/01/2025";
-
-		const priority = document.createElement("div");
-		priority.classList.add("priority");
-		priority.innerText = "High Priority";
-
-		const settings = document.createElement("div");
-		const dotImage = document.createElement("img");
-		settings.classList.add("settings");
-		dotImage.src = dots;
-
-		task.appendChild(title);
-		task.appendChild(description);
-		task.appendChild(dueDate);
-		task.appendChild(priority);
-		task.appendChild(settings);
-		settings.appendChild(dotImage);
-		TASKCONTAINER.appendChild(task);
+		RENDERER.renderNew(title, description, dueDate, priority);
 	};
 	// change priority
 	// delete task
@@ -53,5 +28,41 @@ function Task() {
 
 function Renderer() {
 	// Write setup form to screen
+	const renderNew = (title, description, dueDate, priority) => {
+		const TASK = document.createElement("div");
+		TASK.classList.add("task");
+		TASK.classList.add("high");
+
+		const TITLE = document.createElement("div");
+		TITLE.classList.add("title");
+		TITLE.innerText = title;
+
+		const DESCRIPTION = document.createElement("div");
+		DESCRIPTION.classList.add("description");
+		DESCRIPTION.innerText = description;
+
+		const DUEDATE = document.createElement("div");
+		DUEDATE.classList.add("due-date");
+		DUEDATE.innerText = dueDate;
+
+		const PRIORITY = document.createElement("div");
+		PRIORITY.classList.add("priority");
+		PRIORITY.innerText = priority;
+
+		const SETTINGS = document.createElement("div");
+		const DOTIMAGE = document.createElement("img");
+		SETTINGS.classList.add("settings");
+		DOTIMAGE.src = dots;
+
+		TASK.appendChild(TITLE);
+		TASK.appendChild(DESCRIPTION);
+		TASK.appendChild(DUEDATE);
+		TASK.appendChild(PRIORITY);
+		TASK.appendChild(SETTINGS);
+		SETTINGS.appendChild(DOTIMAGE);
+		TASKCONTAINER.appendChild(TASK);
+	};
 	// Write tasks to screen
+
+	return { renderNew };
 }
